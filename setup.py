000ea0143aa20df79ac541698964f326c90b9ad1
Ambrose J. Carr <ambrosejcarr@gmail.com>
2017-11-16 15:49:05 -0500

    removed unused entrypoint

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
     entry_points={
             'console_scripts': [
                 'Attach10xBarcodes = sctools.platform:TenXV2.attach_barcodes'
-                'Correct10xCellBarcodesHash = sctools.platform:TenXV2.simple_correct_cell_barcodes'
             ]
     },
     classifiers=CLASSIFIERS,
